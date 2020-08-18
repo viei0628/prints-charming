c31847502fbc33294c5919da3bdfd8b2b04139ee
Lenoy Christy <lenoy.christy@mail.mcgill.ca>
2020-08-18 13:21:06 -0400

    added basic controller methods, still need to test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  def index 
+    @printer = Printer.find(params[:printer_id])
+    @bookings = @printer.bookings
+  end
+
+  def new 
+    @printer = Printer.find(params[:printer_id])
+    @booking = Booking.new
+  end
+
+  def create 
+    @booking = Booking.new(booking_params)
+  end
+
+  def booking_params
+    params.require(:booking).permit(:meeting_time, :status)
+  end
 end
