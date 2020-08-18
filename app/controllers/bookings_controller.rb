e6c657099fbc377b455b696ddd9d661e55629405
Lenoy Christy <lenoy.christy@mail.mcgill.ca>
2020-08-18 15:10:18 -0400

    Finished booking controller and booking views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,9 +11,46 @@ class BookingsController < ApplicationController
 
   def create 
     @booking = Booking.new(booking_params)
+    @booking.printer = Printer.find(params[:printer_id])
+    @booking.user = current_user 
+    @booking.status = "Pending"
+    if @booking.save 
+      redirect_to booking_path(@booking)
+    else 
+      render :new
+    end
   end
 
+  def show
+    @booking = Booking.find(params[:id])
+    @printer = @booking.printer
+    @user = @printer.user
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.status = params[:booking][:status]
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @printer = Booking.find(params[:id]).printer
+    Booking.destroy(params[:id])
+    redirect_to printer_bookings_path(@printer)
+  end
+
+  private 
   def booking_params
     params.require(:booking).permit(:meeting_time, :status)
   end
+
+  
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :printer
   validates :meeting_time, presence: true
   validates :status, presence: true
+  validates :status, inclusion: {in: ["Pending", "Accepted", "Rejected"]}
 end
