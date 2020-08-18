7664a8393a36409fafce2988d4268324b2e905df
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-18 12:59:35 -0400

    Created new, create, delete methods for printers_controller and created new.html.erb

--- app/controllers/printers_controller.rb
+++ app/controllers/printers_controller.rb
@@ -2,4 +2,38 @@ class PrintersController < ApplicationController
   def index
     @printers = Printer.all
   end
+
+  def show
+    @printer = Printer.find(params[:id])
+  end
+
+  def new
+    @printer = Printer.new
+  end
+
+  def create
+    @printer = Printer.new(printer_params)
+    # @printer.user_id = current_user
+    if @printer.save
+      redirect_to printer_path(@printer)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    set_printer
+    @printer.destroy
+    redirect_to printers_path
+  end
+
+  private
+
+  def printer_params
+    params.require(:printer).permit(:model, :price)
+  end
+
+  def set_printer
+    @printer = Printer.find(params[:id])
+  end
 end
