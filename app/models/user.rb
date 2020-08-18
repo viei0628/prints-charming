2eecceae108286dd632e12b420839ee60b07a5fe
Nachiket Pusalkar <nachiket87@gmail.com>
2020-08-18 11:22:32 -0400

    added navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :address, presence: true
-  validates :is_owner?, presence: true
 end
