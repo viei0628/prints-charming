01e4eb1ef4e3ab68b251fffc340eb7b665b9593f
Nachiket Pusalkar <nachiket87@gmail.com>
2020-08-17 20:33:15 -0400

    updated forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :address, presence: true
-#  validates :owner, presence: true
+  validates :is_owner?, presence: true
 end
