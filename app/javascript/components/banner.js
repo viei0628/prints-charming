de01218dfcaf38a0fdf38ff3bfd9a328e224bd0e
Nachiket Pusalkar <nachiket87@gmail.com>
2020-08-19 10:42:13 -0400

    fixed typed JS error

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,8 +2,9 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Change your life get a print out now"],
+    strings: ["Change your life", "Get a print out now"],
     typeSpeed: 50,
+    backSpeed: 20,
     loop: true
   });
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,6 +31,8 @@ import { loadDynamicBannerText } from '../components/banner';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  if(document.getElementById("banner-typed-text")){
   loadDynamicBannerText();
+  }
 });
 
