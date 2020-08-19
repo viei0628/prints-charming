b11488bfd9a0a6b2c6009b3f2699a2ab43b619e5
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-19 12:44:36 -0400

    Added styling to printer pages

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -2,6 +2,6 @@ class Printer < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :price, presence: true
-  validates :model, presence: true
+  validates :model, :photo, presence: true
   has_one_attached :photo
 end
