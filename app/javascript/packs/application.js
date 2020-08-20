c7cc4afea263f22da064d781b24e497751e5ee9c
Lenoy Christy <lenoy.christy@mail.mcgill.ca>
2020-08-20 13:48:47 -0400

    got flatpickr back and changed some button styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("flatpickr/dist/flatpickr.css")
+require("flatpickr/dist/themes/material_green.css")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
