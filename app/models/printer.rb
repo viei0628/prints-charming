9ac43fb72cde7303673492cd232e8959cc4afc41
Nachiket Pusalkar <nachiket87@gmail.com>
2020-08-20 14:46:52 -0400

    fixed printer form address bug

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -3,7 +3,6 @@ class Printer < ApplicationRecord
   has_many :bookings
   validates :price, presence: true
   validates :model, :photo, presence: true
-  validates :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_one_attached :photo
