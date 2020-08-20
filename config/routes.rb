53cbb0d6d36b1037fed03aadbd0c2f5d09468841
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-20 14:09:15 -0400

    Added references in models

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :printers, only: [:index, :show, :new, :create, :destroy] do 
+  resources :printers, only: [:index, :show, :new, :create, :destroy] do
     resources :bookings, only: [:index, :new, :create]
+    resources :reviews, only: [:index, :new, :create]
   end
   resources :bookings, only: [:show, :edit, :update, :destroy]
 
